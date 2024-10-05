b3fac5ecdb26f428add02832503860a967122b0f
GyoedCrespo <GJCrespo@student.fullsail.edu>
2024-10-05 13:02:58 -0400

    Pause Menu (No Buttons)

--- fps-project/Assets/Scripts/gameManager.cs
+++ fps-project/Assets/Scripts/gameManager.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class gameManager : MonoBehaviour
 {
@@ -11,10 +12,13 @@ public class gameManager : MonoBehaviour
 
     public bool isPaused;
 
+    float OrigTime;
+
     // Start is called before the first frame update
     void Start()
     {
         instance = this;
+        OrigTime = Time.timeScale;
     }
 
     // Update is called once per frame
@@ -22,9 +26,35 @@ public class gameManager : MonoBehaviour
     {
         if(Input.GetButtonDown("Cancel"))
         {
-            isPaused = !isPaused;
-            menuActive = menuPause;
-            menuActive.SetActive(isPaused);
+            if (menuActive == null)
+            {
+                Paused();
+                menuActive = menuPause;
+                menuActive.SetActive(isPaused);
+            }
+            else if (menuActive == menuPause) {
+                UnPaused();
+              
+            }
         }
+    }
+    public void Paused()
+    {
+        isPaused=!isPaused;
+        Time.timeScale = 0f;
+        Cursor.visible = true;
+        Cursor.lockState = CursorLockMode.Confined;
+    }
+    
+    public void UnPaused()
+    {
+        isPaused= !isPaused;
+        Time.timeScale = OrigTime;
+        Cursor.visible= false;
+        Cursor.lockState = CursorLockMode.Locked;
+        menuActive.SetActive(false);
+        menuActive= null;
+        
+
     }
 }
