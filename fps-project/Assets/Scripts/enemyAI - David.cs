6dd8ac53c281d2cc1b142292910d2f477ba0f1c6
David Oross <DJOross@student.fullsail.edu>
2024-10-05 17:46:42 -0400

    enemyAI update
    added the ability for AI to shoot bullets and added damage class

--- fps-project/Assets/Scripts/enemyAI - David.cs
+++ fps-project/Assets/Scripts/enemyAI - David.cs
@@ -1,13 +1,24 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class enemyAI : MonoBehaviour, IDamage
 {
 
-
+    [SerializeField] NavMeshAgent agent;
     [SerializeField] Renderer model;
+    [SerializeField] Transform shootPos;
+    [SerializeField] Transform headPos;
+
     [SerializeField] int HP;
+
+    [SerializeField] GameObject bullet;
+    [SerializeField] float shootRate;
+
+    bool isShooting;
+    bool playerInRange;
 
     Color colorOrig;
 
@@ -21,7 +32,48 @@ public class enemyAI : MonoBehaviour, IDamage
     // Update is called once per frame
     void Update()
     {
-        
+
+        if (playerInRange)
+        {
+
+            //agent.SetDestination(gameManager.instance.player.transform.position);
+
+            if (!isShooting)
+            {
+                StartCoroutine(shoot());
+            }
+        }
+    }
+
+
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            playerInRange = true;
+        }
+    }
+
+
+    void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            playerInRange = false;
+        }
+    }
+
+
+    IEnumerator shoot()
+    {
+        isShooting = true;
+
+        Instantiate(bullet, shootPos.position, transform.rotation);
+        yield return new WaitForSeconds(shootRate);
+
+
+        isShooting = false;
     }
 
 
