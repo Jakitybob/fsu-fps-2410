5df15407cb4d9b8306ebd436599c7cb43c1422b4
Michael Bump <roanger@yahoo.com>
2024-11-01 08:34:54 +0800

    small beginings fore lore item implemented in my sandbox

---
+++ fps-project/Assets/Scripts/LoreObject.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LoreObject : MonoBehaviour, IInteractable
+{
+    public string loreText;
+    public GameObject lorePanel;
+    public TMPro.TextMeshProUGUI loreTextDisplay;
+
+    private float displayTime = 5f;
+
+    public void Interact(Interact interactor)
+    {
+        
+        
+        // Display lore text using a UI panel
+        lorePanel.SetActive(true);
+        loreTextDisplay.text = loreText;
+        StartCoroutine(ClosePanelAfterTime());
+        
+    }
+    private IEnumerator ClosePanelAfterTime()
+    {
+        yield return new WaitForSeconds(displayTime);
+        lorePanel.SetActive(false);
+        loreTextDisplay.text = "";
+    }
+
+}
