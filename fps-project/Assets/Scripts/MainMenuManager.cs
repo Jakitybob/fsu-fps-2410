8784c6f246da7a994227ce86f195dfb00f17b274
David Oross <DJOross@student.fullsail.edu>
2024-11-07 12:08:11 -0500

    Main Menu Update
    created settings menu option in main menu + added ability to navigate main menu with arrow keys

---
+++ fps-project/Assets/Scripts/MainMenuManager.cs
@@ -0,0 +1,54 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class MainMenuManager : MonoBehaviour
+{
+
+    [SerializeField] GameObject mainMenuActive;
+    [SerializeField] GameObject menuStarting;
+    [SerializeField] GameObject menuSettings;
+
+    bool inSettings;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        mainMenuActive = menuStarting;
+        inSettings = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (EventSystem.current.currentSelectedGameObject == null)
+        {
+            selectStartingButton();
+        }
+    }
+
+
+    void selectStartingButton()
+    {
+        //first button should always be first child of menu
+        EventSystem.current.SetSelectedGameObject(mainMenuActive.transform.GetChild(0).gameObject);
+    }
+
+
+    public void swapMenu()
+    {
+        inSettings = !inSettings;
+
+        if (true == inSettings)
+        {
+            mainMenuActive = menuSettings;
+        }
+        else
+        {
+            mainMenuActive = menuStarting;
+        }
+
+        EventSystem.current.SetSelectedGameObject(null);
+    }
+}
