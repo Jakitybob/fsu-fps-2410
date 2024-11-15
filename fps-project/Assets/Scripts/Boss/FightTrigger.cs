8a7121271867cdf14e1977d13fce656ba17ec863
David Oross <DJOross@student.fullsail.edu>
2024-11-15 09:07:50 -0500

    Level 4 update
    player is now locked in room with boss when starting fight

--- fps-project/Assets/Scripts/Boss/FightTrigger.cs
+++ fps-project/Assets/Scripts/Boss/FightTrigger.cs
@@ -7,19 +7,20 @@ public class FightTrigger : MonoBehaviour
 
     [SerializeField] GameObject bossPrefab;
     [SerializeField] Transform spawnPoint;
-    //
-    //[SerializeField] List<GameObject> spawners
-    //
+
+    [SerializeField] GameObject door;
+    [SerializeField] Transform doorPos;
 
 
 
     private void OnTriggerEnter(Collider other)
     {
+        //spawn boss
         Instantiate(bossPrefab, spawnPoint.position, spawnPoint.localRotation);
 
-        //
-        // turn on spawners here
-        //
+        //spawn locked door
+        Instantiate(door, doorPos.transform.position, doorPos.transform.localRotation);
+
 
         //don't want trigger to stay and respawn bosses everytime you enter it
         Destroy(gameObject);
